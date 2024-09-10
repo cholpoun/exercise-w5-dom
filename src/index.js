2a5d4699344043425d236017e0651f5884d9dd3d
Cholpon <cholpoun@gmail.com>
2024-09-10 19:14:36 +0200

    solved the problem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log("Start Here 😉");
+
+
+
+// // Get references to the radio buttons and the box element
+const darkRadio = document.getElementById('dark');
+const lightRadio = document.getElementById('light');
+const crazyRadio = document.getElementById('crazy');
+const box = document.getElementById('boxThatChangesColor');
+
+
+const changeBoxColor = (theme) => {
+    switch (theme) {
+        case 'dark':
+            box.style.backgroundColor = 'gray';
+            break;
+        case 'light':
+            box.style.backgroundColor = 'red';
+            break;
+        case 'crazy':
+            box.style.backgroundColor = 'green';
+    }
+};
+
+// Add event listeners   
+// to the radio buttons
+darkRadio.addEventListener('change', () => changeBoxColor('dark'));
+lightRadio.addEventListener('change', () => changeBoxColor('light'));
+crazyRadio.addEventListener('change', () => changeBoxColor('crazy'));
+
+// Initial color based on the default checked radio button
+// changeBoxColor('dark');
